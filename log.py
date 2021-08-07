c4c677cf054b03a569c6216f579a81554c75ec17
philip.lourandos <philip.lourandos@gmail.com>
2021-08-07 14:13:30 +0200

    * Added additional parameter to supply log file with list of dates when you were at the office * Added file with the public holiday dates to output tothe generated file if the dat in the normal work week was a public holiday otherwise SARS might think you've put no effort into this

--- log.py
+++ log.py
@@ -5,21 +5,49 @@ import argparse
 import io
 import sys
 
+from jsonpath_ng import jsonpath, parse
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--start', help = 'Start date of WFH. ISO8601 date format')
 parser.add_argument('--end', help = 'End date of WFH. ISO8601 date format')
+parser.add_argument('--office', help = 'File containing ISO-8601 dates when present at the office')
 
 args = parser.parse_args()
 
 start = arrow.get(args.start)
 end = arrow.get(args.end)
+officeFile = args.office
+
+holidays = []
+office = []
+
+if officeFile is not None:
+    with open(officeFile, 'r') as at_office:
+        file_content = at_office.readlines()
+
+        for office_date in file_content:
+            office.append(office_date.strip())
+
+with open('holidays.txt', 'r') as publicHolidays:
+    file_content = publicHolidays.readlines()
+
+    for current_line in file_content:
+        holidays.append(current_line.strip())
 
 with open('wfh.csv', 'w') as schedule:
-    schedule.write('Date, Day, Location, Comments\n')
+    schedule.write('Date, Day, Location\n')
+
 
     for current_date in arrow.Arrow.span_range('day', start, end):
         date = current_date[0]
 
-        if(date.isoweekday() in range(1,6)):
-            schedule.write(date.format("YYYY-MM-DD") + "," + date.format("dddd") + ", Home Office,,\n")
+        if date.isoweekday() in range(1,6):
+            str_date = date.format("YYYY-MM-DD")
+
+            if str_date in holidays:
+                schedule.write(str_date + "," + date.format("dddd") + ",Public Holiday\n")
+            elif str_date in office:
+                schedule.write(str_date + "," + date.format("dddd") + ",Working at employers office\n")
+            else:
+                schedule.write(str_date + "," + date.format("dddd") + ",Working from home\n")
 
